1a2fef68977d95752f41a9e65f5a7d11a81efc39
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-01-31 00:18:19 +0100

    added onSubmit + types

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import {
   Card,
   CardContent,
@@ -6,20 +8,43 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Form } from '@/components/ui/form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8),
+  passwordConfirm: z.string(),
+});
 
 export default function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+    },
+  });
+
+  const handleValidatedSubmit = async (data: z.infer<typeof formSchema>) => {};
+
   return (
     <main className="flex justify-center items-center min-h-screen">
-      <Card>
+      <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle>Register for TODOING</CardTitle>
+          <CardDescription>Register for a new account.</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleValidatedSubmit)}></form>
+          </Form>
         </CardContent>
         <CardFooter>
-          <p>Card Footer</p>
+          <p></p>
         </CardFooter>
       </Card>
     </main>
