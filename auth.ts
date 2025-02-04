bca1f32b078bdcf88bac146a96e107bd9dcf5bfc
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-02-04 16:35:18 +0100

    configured authjs

--- auth.ts
+++ auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        username: {},
+      },
+    }),
+  ],
 });
