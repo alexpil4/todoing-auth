c5fca29154d5849f7e8eb7d77dba9a115989d3b6
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-02-07 11:59:26 +0100

    added route group for logged in pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { auth } from '@/auth';
-import LogoutButton from './logoutButton';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -24,21 +22,9 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div>
-          {session?.user?.email ? (
-            <div>
-              {session.user.email} | <LogoutButton />
-            </div>
-          ) : (
-            'Not currently logged in user'
-          )}
-        </div>
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
