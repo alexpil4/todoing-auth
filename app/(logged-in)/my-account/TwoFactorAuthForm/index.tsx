5e6d3e5e5ae7f04d98d5463d03d94cdb394bae1a
Alessandro Pilastrini <133789773+apilastrini@users.noreply.github.com>
2025-02-20 16:06:46 +0100

    completed QRCode scan

--- app/(logged-in)/my-account/TwoFactorAuthForm/index.tsx
+++ app/(logged-in)/my-account/TwoFactorAuthForm/index.tsx
@@ -4,39 +4,69 @@ import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 import { get2FASecret } from './action';
 import { useToast } from '@/hooks/use-toast';
+import { QRCodeSVG } from 'qrcode.react';
 
 type Props = {
   is2FAActivated: boolean;
 };
 
 export default function TwoFactorAuthForm({ is2FAActivated }: Props) {
-  const { toast } = useToast;
+  const { toast } = useToast();
 
   const [isActivated, setIsActivated] = useState(false);
   const [step, setStep] = useState(1);
+  const [code, setCode] = useState('');
 
   useEffect(() => {
     setIsActivated(is2FAActivated);
   }, [is2FAActivated]);
 
   const handleEnable2FA = async () => {
-    setStep(2);
     const response = await get2FASecret();
 
     if (response.error) {
+      toast({
+        variant: 'destructive',
+        title: response.message,
+      });
+      return;
     }
+    setStep(2);
+    setCode(response.twoFactorSecret ?? '');
+  };
+
+  const goToStep = (step: number) => {
+    setStep(step);
   };
 
   return (
     <>
       {!isActivated && (
-        <div>
+        <div className="mt-4">
           {step === 1 && (
             <Button className="uppercase" onClick={handleEnable2FA}>
               Enable Two-Factor Authentication
             </Button>
           )}
-          {step === 2 && <div>show QR code</div>}
+          {step === 2 && (
+            <div>
+              <p className="text-xs text-muted-foreground py-4">
+                Scan the QR code below in the Goggle Authenticator app to activate Two-Factor
+                Authentication.
+              </p>
+              <QRCodeSVG className="w-full" value={code} />
+              <Button onClick={() => goToStep(3)} className="uppercase mt-8 w-full">
+                I have scanned the QR code
+              </Button>
+              <Button
+                className="uppercase w-full mt-4"
+                variant="outline"
+                onClick={() => goToStep(1)}
+              >
+                Cancel
+              </Button>
+            </div>
+          )}
         </div>
       )}
     </>
